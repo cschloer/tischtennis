e0a2d8a4ba5979f2bba01ba235447bd15a01d457
Conrad S <conrad.schloer@gmail.com>
2022-12-17 15:49:15 +0100

    push score

--- tischtennis/code/database/database.go
+++ tischtennis/code/database/database.go
@@ -2,14 +2,12 @@ package database
 
 import (
 	"github.com/aws/aws-sdk-go/aws"
-	"gonum.org/v1/gonum/mat"
 
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 	"github.com/rs/xid"
-	"math"
 	"os"
 	"time"
 
@@ -37,7 +35,7 @@ type Game struct {
 }
 
 type Score struct {
-	MonthYear string
+	YearMonth string
 	Created   int64
 	ScoresMap map[string]float64
 }
@@ -73,7 +71,10 @@ var ENVIRONMENT = os.Getenv("ENVIRONMENT")
 
 func _createId() string {
 	return xid.New().String()
-
+}
+func _getNow() (int64, time.Time) {
+	timeObj := time.Now()
+	return timeObj.UnixNano(), timeObj
 }
 
 func _getTableName(table string) string {
@@ -234,7 +235,7 @@ func CreatePerson(name string, faIcon string, accessKey string) (personId string
 }
 
 func CreateGame(reporterId string, otherPersonId string, wins int32, losses int32) (personId1 string, personId2 string, created int64, err error) {
-	created = time.Now().UnixNano()
+	created, _ = _getNow()
 	game1 := Game{
 		PersonId:      reporterId,
 		Created:       created,
@@ -583,184 +584,6 @@ func DeleteGame(personId string, created int64) (personId1 string, personId2 str
 
 	return personId, game.OtherPersonId, created, nil
 }
-func ComputeScores() (insertedId string, err error) {
-	// TODO we can compute the eigenvector using the power method
-	// https://www.jstor.org/stable/2132526
-	people, err := GetPeople()
-	if err != nil {
-		return insertedId, err
-	}
-
-	invalidPeople := make([]Person, 0)
-	// People with at least 1 win
-	validPeople := make([]Person, 0)
-	for _, p := range people {
-		if p.Wins+p.Losses > 0 {
-			validPeople = append(validPeople, p)
-		} else {
-			invalidPeople = append(invalidPeople, p)
-		}
-	}
-
-	scoresMaps := make(map[string]float64)
-	transactions := make([]*dynamodb.TransactWriteItem, len(validPeople)+len(invalidPeople))
-	if len(validPeople) > 0 {
-
-		recordMatrix := make([][]float64, len(validPeople))
-		// A map mapping person ID to index
-		personIdMap := map[string]int{}
-
-		for i, p := range validPeople {
-			col := make([]float64, len(validPeople))
-			for j := range col {
-				if j == i {
-					// No games played against yourself
-					col[j] = 0
-				} else {
-					// We artifically give everyone 0.1 win against eachother
-					// to prevent issues with undefeated players forcing everyone to 0
-					col[j] = 0.1
-				}
-			}
-			recordMatrix[i] = col
-			personIdMap[p.Id] = i
-		}
-
-		if err != nil {
-			return insertedId, err
-		}
-
-		for _, person := range validPeople {
-			gamesMap, err := GetGames([]Person{person}, -1)
-			if err != nil {
-				return insertedId, err
-			}
-			for _, game := range gamesMap[person.Id] {
-				p1Index, ok1 := personIdMap[game.PersonId]
-				p2Index, ok2 := personIdMap[game.OtherPersonId]
-				if ok1 && ok2 {
-					// Fill up the record matrix with wins only for the current person
-					recordMatrix[p1Index][p2Index] = recordMatrix[p1Index][p2Index] + float64(game.Wins)
-				}
-			}
-		}
-
-		// Create a record matrix that is weighted by number of games played
-		data := make([]float64, len(validPeople)*len(validPeople))
-		for i, p := range validPeople {
-			totalGames := p.Wins + p.Losses
-			for j, _ := range validPeople {
-				index := i*len(validPeople) + j
-				// We divide by total games + len(validPeople) because we've artifically added 1 win
-				// against everyone in order to prevent undefeated players from setting everyone to 0
-				data[index] = recordMatrix[i][j] / float64(totalGames+int32(len(validPeople)))
-			}
-		}
-
-		a := mat.NewDense(len(validPeople), len(validPeople), data)
-		var eig mat.Eigen
-		ok := eig.Factorize(a, mat.EigenRight)
-		if !ok {
-			return insertedId, errors.New("Eigendecomposition failed")
-		}
-		eigenvalues := eig.Values(nil)
-		eigenvectors := mat.NewCDense(len(validPeople), len(validPeople), nil)
-		eig.VectorsTo(eigenvectors)
-
-		largestEigenvalue := real(eigenvalues[0])
-		largestEigenvalueIndex := 0
-		for i, _ := range validPeople {
-			realValue := real(eigenvalues[1])
-			if realValue > largestEigenvalue {
-				largestEigenvalue = realValue
-				largestEigenvalueIndex = i
-			}
-		}
-		scores := make([]float64, len(validPeople))
-		for j, _ := range validPeople {
-			scores[j] = math.Abs(real(eigenvectors.At(j, largestEigenvalueIndex)))
-		}
-
-		for i, p := range validPeople {
-			scoresMap[p.Id] = scores[i]
-			transactions[i] = &dynamodb.TransactWriteItem{
-				Update: &dynamodb.Update{
-					ExpressionAttributeNames: map[string]*string{
-						"#Score": aws.String("Score"),
-					},
-					ExpressionAttributeValues: map[string]*dynamodb.AttributeValue{
-						":score": {
-							N: aws.String(fmt.Sprintf("%f", scores[i])),
-						},
-					},
-					TableName: aws.String(_getTableName("person")),
-					Key: map[string]*dynamodb.AttributeValue{
-						"Id": &dynamodb.AttributeValue{
-							S: aws.String(p.Id),
-						},
-					},
-					UpdateExpression: aws.String(
-						"SET #Score = :score",
-					),
-				},
-			}
-		}
-		/*
-			// PRINT states for future debugging
-				fmt.Println("Valid people: ", validPeople)
-				fmt.Println("Record matrix: ", recordMatrix)
-				fmt.Println("Weighted data: ", data)
-				fmt.Printf("Eigenvalues:\n%v\n", eigenvalues)
-				fmt.Println("Eigenvectors: ")
-				for i, _ := range validPeople {
-					for j, _ := range validPeople {
-						floatValue := real(eigenvectors.At(i, j))
-						fmt.Printf("%f ", floatValue)
-					}
-					fmt.Printf("\n")
-				}
-				fmt.Println("Largest eigenvalue index: ", largestEigenvalueIndex)
-				fmt.Println("Final scores: ", scores)
-		*/
-
-	}
-
-	// Set invalid people scores to -1
-	for i, p := range invalidPeople {
-		scoresMap[p.Id] = -1.0
-		transactions[1 + len(validPeople)+i] = &dynamodb.TransactWriteItem{
-			Update: &dynamodb.Update{
-				ExpressionAttributeNames: map[string]*string{
-					"#Score": aws.String("Score"),
-				},
-				ExpressionAttributeValues: map[string]*dynamodb.AttributeValue{
-					":score": {
-						N: aws.String(fmt.Sprintf("%f", -1.0)),
-					},
-				},
-				TableName: aws.String(_getTableName("person")),
-				Key: map[string]*dynamodb.AttributeValue{
-					"Id": &dynamodb.AttributeValue{
-						S: aws.String(p.Id),
-					},
-				},
-				UpdateExpression: aws.String(
-					"SET #Score = :score",
-				),
-			},
-		}
-	}
-		transactions[] = &dynamodb.TransactWriteItem{
-
-		}
-	err = _loopTransactions(transactions)
-	if err != nil {
-		return insertedId, err
-	}
-
-	return insertedId, nil
-}
-
 func AdminDatabase() (res string, err error) {
 	person1, _ := CreatePerson("dev_Lucas", "fas fa-chess-knight", "123")
 	person2, _ := CreatePerson("dev_Conrad", "fas fa-water", "123")
@@ -779,7 +602,23 @@ func AdminDatabase() (res string, err error) {
 	CreateGame(person1, person3, 10, 10)
 	CreateGame(person2, person3, 11, 11)
 
-	_, err = ComputeScores()
+	scanInput := &dynamodb.ScanInput{
+		TableName: aws.String(_getTableName("score")),
+	}
+	result, err := svc.Scan(scanInput)
+	if err != nil {
+		return person3, err
+	}
+	for _, item := range result.Items {
+		score := Score{}
+
+		err = dynamodbattribute.UnmarshalMap(item, &score)
+
+		if err != nil {
+			return person3, err
+		}
+		fmt.Println("SCORE", score)
+	}
 
 	return person3, err
 
--- tischtennis/static/js/base.js
+++ tischtennis/static/js/base.js
@@ -8,15 +8,16 @@ const checkRes = async (res) => {
   return true;
 };
 
-const copyToClipboard = (val) => {
-  console.log("INSIDE COPY TO CLIPBOARD");
-  var dummy = document.createElement("input");
-  dummy.style.display = "none";
+function copyToClipboard(text) {
+  var dummy = document.createElement("textarea");
+  // to avoid breaking orgain page when copying more words
+  // cant copy when adding below this code
+  // dummy.style.display = 'none'
   document.body.appendChild(dummy);
-
-  dummy.setAttribute("id", "dummy_id");
-  document.getElementById("dummy_id").value = val;
+  //Be careful if you use texarea. setAttribute('value', value), which works with "input" does not work with "textarea". – Eduard
+  dummy.value = text;
   dummy.select();
   document.execCommand("copy");
   document.body.removeChild(dummy);
-};
+  return false;
+}
